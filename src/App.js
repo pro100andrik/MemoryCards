e703fe48b1cceabf7d2572803b820464c4838251
Andriy Konzelskiy <pro100andrik94@gmail.com>
2022-09-02 11:43:33 +0300

    now timer and moves counter working, added win message

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
       cards: this.shufleCards([...structuredClone(defaultCards).slice(0, 8), ...structuredClone(defaultCards).slice(0, 8)]),
       mode: "4x4",
       currentMove: 'first',
+      gameStatus: 'waiting',
     })
   }
 
@@ -52,11 +53,18 @@ class App extends React.Component {
 
   restatrGame = () => {
     this.closeAllCards();
+
+    this.setState ({
+      currentMove: 'first',
+      time: 0,
+      gameStatus: 'waiting',
+      moves: 0,
+    })
+
     setTimeout(() => {
       this.setState ({
         cards: this.shufleCards(this.state.cards),
         clickCooldown: false,
-        currentMove: 'first',
       })
     }, 500);
     console.log('restartGame')
@@ -65,6 +73,21 @@ class App extends React.Component {
   handleCardClick = (cardIndex) => {
     if(this.state.cards[cardIndex].opened){
       return
+    }
+    if (this.state.gameStatus === 'waiting') {
+      this.setState({
+        gameStatus: 'started'
+      })
+      const timerInterval = setInterval(() => {
+        if (this.state.gameStatus === 'win' || this.state.gameStatus === 'waiting'){
+          clearInterval(timerInterval)
+        }else{
+          this.setState({
+            time: this.state.time + 1
+          })
+        }
+      },1000)
+
     }
     if (this.state.currentMove === 'first'){
       if (this.state.clickCooldown === true){
@@ -81,17 +104,34 @@ class App extends React.Component {
         secondOpenedCardId: this.state.cards[cardIndex].id,
         secondOpenedCardIndex: cardIndex,
         clickCooldown: true,
+        moves: this.state.moves + 1,
       },this.openCardByIndex(cardIndex))
       setTimeout(() => {
         if (this.state.firstOpenedCardId !== this.state.secondOpenedCardId){
           this.closeCardsByIndex(this.state.firstOpenedCardIndex, this.state.secondOpenedCardIndex);
         }else{
-          this.resetCooldown()
+          this.resetCooldown();
+          this.checkForWin();
         }
         // console.log(this.state.firstOpenedCardId,this.state.secondOpenedCardId)
       }, 1);
     }
 
+  }
+
+  checkForWin = () => {
+    const closedCardsAmount = this.state.cards.reduce((amount, card) => {
+      if (!card.opened){
+        amount += 1;
+      }
+      return amount;
+    }, 0)
+    if (closedCardsAmount === 0){
+      this.setState({
+        gameStatus: 'win',
+      })
+      console.log('win')
+    }
   }
 
   resetCooldown = () => {
@@ -118,7 +158,7 @@ class App extends React.Component {
         cards: this.state.cards.map((card, index) => {
           if (firstOpenedCardIndex === index || secondOpenedCardIndex === index){
             // console.log(cardIndex , index)
-            card.opened = !card.opened
+            card.opened = false
           };
           return card;
         })
@@ -129,16 +169,25 @@ class App extends React.Component {
   render(){
     // console.log(this.state.cards)
     return(
-      <div className="wrapper">
-        <div className="title-box">Memory Cards Game</div>
-        <InfoControls  time={this.state.time}
-          moves={this.state.moves}
-          restartGame={this.restatrGame}
-          showSettings={this.showSettings}/>
-        <GameBoard cards={this.state.cards}
-          mode={this.state.mode}
-          handleCardClick={this.handleCardClick}/>
-      </div>
+      <>
+
+        <div className="wrapper">
+          {this.state.gameStatus === "win"
+          ?
+          <div className='win-message'>Congrats! You win!</div>
+          :
+          null}
+
+          <div className="title-box">Memory Cards Game</div>
+          <InfoControls  time={this.state.time}
+            moves={this.state.moves}
+            restartGame={this.restatrGame}
+            showSettings={this.showSettings}/>
+          <GameBoard cards={this.state.cards}
+            mode={this.state.mode}
+            handleCardClick={this.handleCardClick}/>
+        </div>
+      </>
     )
   }
 }
