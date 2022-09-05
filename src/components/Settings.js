c3f43a05fa7b4cf1dbb1051a67854b72154f40af
Andriy Konzelskiy <pro100andrik94@gmail.com>
2022-09-05 12:24:58 +0300

    Added settings component

---
+++ src/components/Settings.js
@@ -0,0 +1,23 @@
+const Settings = props => {
+  return (
+    <>
+      <div>Settings</div>
+      <div> Mode: {props.modes.map((element, index) => {
+        return <Button key={index} mode={element} prevMode={props.mode} changeMode={props.changeMode}/>
+      })}</div>
+      <div>Hardcore mode:
+        <button onClick={props.toggleHardcore}
+                className={props.hardcore ? 'hardcore-button-on' : 'hardcore-button-off'}>{props.hardcore ? 'On' : 'Off'}</button></div>
+    </>
+  )
+}
+
+const Button = props => {
+  return (
+    <button className='mode-button chosen'
+      disabled={props.prevMode === props.mode}
+      onClick={() => props.changeMode(props.mode)}>{props.mode}</button>
+  )
+}
+
+export default Settings
