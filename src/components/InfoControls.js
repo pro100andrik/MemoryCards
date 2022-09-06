732b00601f0ae0167bce8651fe902f5e293defe2
Andriy Konzelskiy <pro100andrik94@gmail.com>
2022-09-06 11:28:31 +0300

    changed shown time to minuts and seconds

--- src/components/InfoControls.js
+++ src/components/InfoControls.js
@@ -1,8 +1,12 @@
 
+function formatTime(seconds) {
+  return ('0'+parseInt(seconds / 60 % 60)).slice(-2) + ':' + ('0'+parseInt(seconds % 60)).slice(-2)
+}
+
 const InfoControls = (props) => {
   return(
     <div className="info-controls-box">
-      <div>Time: {props.time} </div>
+      <div>Time: {formatTime(props.time)} </div>
       <div>Moves: {props.moves} </div>
       <div><button className='button' onClick={props.restartGame}>Restart</button></div>
       <div><button className='button' onClick={props.showSettings}>Settings</button></div>
